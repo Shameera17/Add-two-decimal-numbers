eef18e7518efdb3e7060841f068f7737c6ca79a4
siara989 <siara.roxanna@gmail.com>
2018-11-11 10:07:02 +0530

    add a code written in c language

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float no1,no2,sum;
+    printf("Hello This is program to ADD TWO DECIMAL VALUES!\n");
+    printf("Enter first number\n");
+    scanf("%f",&no1);
+    printf("Enter second number\n");
+    scanf("%f",&no2);
+    sum=no1+no2;
+    printf("The sum is:\t%f",sum);
+
+    return 0;
+}
